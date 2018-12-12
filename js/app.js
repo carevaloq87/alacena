6eb4961ac68ab6ad23daae1d87172bbbb9b65b07
Christian Arevalo <carevaloq87@gmail.com>
2018-12-12 21:41:52 +1100

    Mostrar los nombres de los productos

--- js/app.js
+++ js/app.js
@@ -1,36 +1,53 @@
 
-        var app = new Vue({
-            el: '#app',
-            data: {
-                niveles: [],
-                tipo_productos: [],
-                url_niveles: './dataset/nivel.json',
-                url_tipo_productos: './dataset/tipo_producto.json'
-            },
-            methods: {
-                cargar_dataset: function () {
-                    let self = this;
-                    axios.get(self.url_niveles)
-                        .then(function (response) {
-                            console.log(response);
-                            self.niveles = response.data;
-                        })
-                        .catch(function (error) {
-                            console.log(error);
-                        });
-                    axios.get(self.url_tipo_productos)
-                        .then(function (response) {
-                            console.log(response);
-                            self.tipo_productos = response.data;
-                        })
-                        .catch(function (error) {
-                            console.log(error);
-                        });
-                        
-                }
-            },
-            mounted(){
-                this.cargar_dataset()
-            }
+Vue.component('productos', {
+    props: ['producto', 'key'],
+    template: `<div class="col-sm-12">
+                        <span v-html="producto.nombre"></span> 
+                </div>`
+})
 
-        });
+var app = new Vue({
+    el: '#app',
+    data: {
+        niveles: [],
+        tipo_productos: [],
+        productos: [],
+        url_niveles: './dataset/nivel.json',
+        url_productos: './dataset/productos.json',
+        url_tipo_productos: './dataset/tipo_producto.json'
+    },
+    methods: {
+        cargar_dataset: function () {
+            let self = this;
+            axios.get(self.url_niveles)
+                .then(function (response) {
+                    console.log(response);
+                    self.niveles = response.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+            axios.get(self.url_tipo_productos)
+                .then(function (response) {
+                    console.log(response);
+                    self.tipo_productos = response.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+                axios.get(self.url_productos)
+                    .then(function (response) {
+                        console.log(response);
+                        self.productos = response.data;
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+                
+        }
+    },
+    mounted(){
+        this.cargar_dataset()
+    }
+
+});
