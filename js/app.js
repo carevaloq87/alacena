0b62451c42b0ca9608475be838df2c768b4f126c
Christian Arevalo <carevaloq87@gmail.com>
2019-01-09 21:58:23 +1100

    Agregar filtros y  mejorar vista de tabla

--- js/app.js
+++ js/app.js
@@ -4,8 +4,19 @@ var app = new Vue({
     el: '#alacena',
     data: {
         niveles: [],
+        texto: 'descrip',
         tipo_productos: [],
+        producto_nuevo: {
+            "id": 1,
+            "nombre": "",
+            "fecha_vencimiento": "",
+            "status": 0,
+            "tipo_producto": 0,
+            "nivel_id": 0
+        },
         productos: [],
+        productos_a_mostrar: [],
+        filtro_actual: -1,
         url_niveles: './dataset/nivel.json',
         url_productos: './dataset/productos.json',
         url_tipo_productos: './dataset/tipo_producto.json'
@@ -43,7 +54,8 @@ var app = new Vue({
             axios.get(self.url_productos)
                 .then(function (response) {
                     console.log(response);
-                    self.productos = response.data;
+                    self.productos_a_mostrar = self.productos = response.data;
+                    
                 })
                 .catch(function (error) {
                     console.log(error);
@@ -68,6 +80,35 @@ var app = new Vue({
             let nombre = '';
             objetos.forEach( objeto => (objeto.id == id ? nombre = objeto.nombre : '') );
             return nombre;
+        },
+        agregar_producto: function() {
+            let pn = Object.assign({}, this.producto_nuevo);
+            this.productos_a_mostrar.push(pn);
+        },
+        borrar_producto: function(id) {
+            this. productos_a_mostrar = this.productos = this.productos.filter(function(prod) {
+                if(prod.id != id){
+                    return prod;
+                }
+            });
+        },
+        filtro_activos: function() {
+            this.filtro_actual++;
+            console.log(this.filtro_actual);
+            this.productos_a_mostrar = this.productos_a_mostrar.filter(function(prod) {
+                console.log(prod);
+                if(parseInt(prod.status) == parseInt(this.filtro_actual)){ 
+                    return prod;
+                }
+            });
+
+            if(this.filtro_actual >= 1){
+                this.filtro_actual = -1;
+            }
+
+        },
+        limpiar_filtros: function() {
+            this.productos_a_mostrar = this.productos;
         }
     },
     computed: {},
